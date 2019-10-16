8f2abd5fd79717ff6cd29e024612607e24bb9da1
fran <franciscojose.aunon@gmail.com>
2019-10-16 14:32:32 +0200

    Ejercicio suma 3 calificaciones

---
+++ 1.-Operadores/src/suma3calificaciones/Main.java
@@ -0,0 +1,25 @@
+package suma3calificaciones;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner entrada=new Scanner(System.in);		
+		float nota1, nota2, nota3, suma;
+		
+		System.out.println("Introduzca 3 calificaciones");
+		nota1=entrada.nextFloat();
+		nota2=entrada.nextFloat();
+		nota3=entrada.nextFloat();
+		
+		suma=nota1+nota2+nota3;
+		
+		System.out.println("La suma es "+suma);
+		
+		
+		
+	}
+
+}
